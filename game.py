757b0738555c1eb57f7e7a51f1e7bbde16e5541b
dpeacockk <dpeacock@umich.edu>
2021-07-08 13:18:55 -0400

    begun game.py and created vir environment

---
+++ game.py
@@ -0,0 +1,12 @@
+#Created by: Daniel Peacock
+
+#The classic Snake Game in Python.
+
+#Rules: Move up, down, left, right with W, S, A, D, respectively.
+#       You cannot run into yourself or the walls, or you must restart.
+#       The objective is to eat apples until you are as long as the entire screen.
+
+import pygame
+
+pygame.display.set_mode()
+
