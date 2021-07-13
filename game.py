977d7804e58a37a953eade5f6b388f3fc755f1e0
dpeacockk <dpeacock@umich.edu>
2021-07-13 12:52:26 -0400

    progress on main menu

--- game.py
+++ game.py
@@ -7,6 +7,37 @@
 #       The objective is to eat apples until you are as long as the entire screen.
 
 import pygame
+import sys
+import time
+import random
+pygame.init()
+pygame.display.set_caption('Snake Game Classic')
+#FOR REFERENCE: pygame.display.set_mode([screen width, screen height])
+screen = pygame.display.set_mode((1000,700))
 
-pygame.display.set_mode()
+print(pygame.font.get_fonts())
+#TEST DIFFERENT FONTS: 'dejavuserif', 'dejavusansmono', 'freesans', 'dejavusans', 'freeserif', 'freemono'
+font = pygame.font.Sysfont(freesans)
 
+#Sets some standard colors for later usage:
+snake_color = (150,240,0)
+menu_text_color = (0,50,0)
+title_color = (150,240,50)
+
+
+#Functions
+#Function to create text on the screen
+def create_text(text, font_input, color,surface,x,y):
+    textobj = font_input.render(text,1,color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x,y)
+    surface.blit(textobj,textrect)
+
+#MAIN MENU
+def main_menu():
+    while True:
+        screen = pygame.image.load('soft_green_back.png').convert()
+        create_text("Snake Game Classic", font, title_color, )
+
+#GAME
+def game_loop():
